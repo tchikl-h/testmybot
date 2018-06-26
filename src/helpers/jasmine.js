30f9cdfeec38fbc3385ed9b23a8892b6b4665ce0
Hervé Tchikladzé <herve.tchikladze@epitech.eu>
2018-06-26 14:02:01 +0200

    Update jasmine.js

--- src/helpers/jasmine.js
+++ src/helpers/jasmine.js
@@ -3,7 +3,7 @@
 const TestMyBot = require('../testmybot')
 const moduleinfo = require('../util/moduleinfo')
 
-const defaultTimeout = 400000
+const defaultTimeout = 150000
 
 module.exports.setupJasmineTestCases = ({ timeout: timeout = defaultTimeout, testcaseSelector, tmb } = {}) => {
   if (!tmb) tmb = new TestMyBot()
